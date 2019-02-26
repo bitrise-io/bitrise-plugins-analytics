bd8e65a82c9be5ff96bfe8a469c4fbbfa8d32ba0
Gödrei Krisztián <godreikrisztian@gmail.com>
2019-02-26 11:02:57 +0100

    import missing test package

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/bitrise-core/bitrise-plugins-analytics/cli"
+import (
+	"github.com/bitrise-core/bitrise-plugins-analytics/cli"
+	_ "github.com/stretchr/testify/require"
+)
 
 func main() {
 	cli.Run()
