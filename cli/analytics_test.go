f8615a18eeb945fc935ceaaa04da80b6ac71fac7
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2020-03-10 15:17:21 +0100

    Use testify package and add STDIN read test

--- cli/analytics_test.go
+++ cli/analytics_test.go
@@ -1,19 +1,49 @@
 package cli
 
 import (
+	"fmt"
 	"io"
 	"os"
-	"path/filepath"
-	"reflect"
+	"os/exec"
 	"strings"
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/bitrise-io/bitrise/models"
-	"github.com/bitrise-io/go-utils/pathutil"
 	"github.com/bitrise-io/go-utils/pointers"
 	stepmanModels "github.com/bitrise-io/stepman/models"
 )
+
+func Test_readPayload(t *testing.T) {
+	tests := []struct {
+		name    string
+		r       io.Reader
+		want    models.BuildRunResultsModel
+		wantErr bool
+	}{
+		{
+			name:    "reading empty payload returns an error (no input provided)",
+			r:       strings.NewReader(""),
+			want:    models.BuildRunResultsModel{},
+			wantErr: true,
+		},
+		{
+			name:    "reading invalid payload returns an error",
+			r:       strings.NewReader("invalid json"),
+			want:    models.BuildRunResultsModel{},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := readPayload(tt.r)
+			require.Equal(t, err != nil, tt.wantErr, fmt.Sprintf("expected error: %v, got: %v", tt.wantErr, err == nil))
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
 
 const failedBuildPayload = `{
 	"stepman_updates":{
@@ -71,63 +101,18 @@ var faildBuildBuildRunResult = models.BuildRunResultsModel{
 	},
 }
 
-func Test_readPayload(t *testing.T) {
-	tests := []struct {
-		name    string
-		r       io.Reader
-		want    models.BuildRunResultsModel
-		wantErr bool
-	}{
-		{
-			name:    "reading an empty string returns an error (no input provided)",
-			r:       strings.NewReader(""),
-			want:    models.BuildRunResultsModel{},
-			wantErr: true,
-		},
-		{
-			name:    "reading invalid payload returns an error",
-			r:       strings.NewReader("invalid json"),
-			want:    models.BuildRunResultsModel{},
-			wantErr: true,
-		},
-		{
-			name:    "can read payload",
-			r:       strings.NewReader(failedBuildPayload),
-			want:    faildBuildBuildRunResult,
-			wantErr: false,
-		},
+func Test_readPayloadFromStdin(t *testing.T) {
+	if os.Getenv("READ_STDIN") == "1" {
+		payload, err := readPayload(os.Stdin)
+		require.NoError(t, err)
+		require.Equal(t, payload, faildBuildBuildRunResult)
+		return
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := readPayload(tt.r)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("readPayload() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("readPayload() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 
-func Test_readPayloadFromEmptyFileFails(t *testing.T) {
-	tmp, err := pathutil.NormalizedOSTempDirPath("_readPayload_")
-	if err != nil {
-		t.Errorf("failed to create tmp dir: %s", err)
-		return
-	}
-	pth := filepath.Join(tmp, "file")
-	f, err := os.Create(pth)
-	if err != nil {
-		t.Errorf("failed to open %s: %s", pth, err)
-		return
-	}
-	payload, err := readPayload(f)
-	if err == nil {
-		t.Errorf("reading empty payload should fail")
-	}
-	if !reflect.DeepEqual(payload, models.BuildRunResultsModel{}) {
-		t.Errorf("readPayload() = %v, want %v", payload, models.BuildRunResultsModel{})
-	}
+	cmd := exec.Command(os.Args[0], "-test.run=Test_readPayloadFromStdin")
+	cmd.Env = append(os.Environ(), "READ_STDIN=1")
+	cmd.Stdin = strings.NewReader(failedBuildPayload)
+
+	err := cmd.Run()
+	require.NoError(t, err)
 }
